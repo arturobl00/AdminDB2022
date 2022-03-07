0d9fb000c58336c3b9e9e2d0e0776e8b8ac04c70
arturobl00 <arturobl00@msn.com>
2022-03-07 15:11:29 -0600

    07/03

--- admindbu2/app.js
+++ admindbu2/app.js
@@ -8,7 +8,7 @@ firebase.initializeApp({
   var db = firebase.firestore();
 
 function agregar(){
-    console.log("Hola llamaste a agregar");
+    //console.log("Hola llamaste a agregar");
 
     var nom = document.getElementById('nombre').value;
     var apell = document.getElementById('apellido').value;
@@ -21,9 +21,31 @@ function agregar(){
     })
     .then((docRef) => {
         console.log("Document written with ID: ", docRef.id);
+        document.getElementById('nombre').value = "";
+        document.getElementById('apellido').value = "";
+        document.getElementById('edad').value = "";
     })
     .catch((error) => {
         console.error("Error adding document: ", error);
     });
 
 }
+
+tabla = document.getElementById('tabla');
+
+db.collection("miColeccion").onSnapshot((querySnapshot) => {
+  tabla.innerHTML = ''; //Limpiar mi tabla
+  querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data().last}`);
+      tabla.innerHTML += `
+      <tr>
+          <th scope="row">${doc.id}</th>
+          <td>${doc.data().Sunombre}</td>
+          <td>${doc.data().Suapellido}</td>
+          <td>${doc.data().Suedad}</td>
+          <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
+          <td><button class="btn btn-warning" onclick="editar('${doc.id}','${doc.data().first}','${doc.data().last}','${doc.data().born}')">Editar</button></td>
+      </tr>
+      `
+      });
+  });
