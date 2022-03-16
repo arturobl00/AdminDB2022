c37947f97823da4713cd1c7ed9b56680f60d635a
arturobl00 <arturobl00@msn.com>
2022-03-16 10:02:21 -0600

    16/03

--- admindbu2/app.js
+++ admindbu2/app.js
@@ -44,8 +44,51 @@ db.collection("miColeccion").onSnapshot((querySnapshot) => {
           <td>${doc.data().Suapellido}</td>
           <td>${doc.data().Suedad}</td>
           <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
-          <td><button class="btn btn-warning" onclick="editar('${doc.id}','${doc.data().first}','${doc.data().last}','${doc.data().born}')">Editar</button></td>
+          <td><button class="btn btn-warning" onclick="editar('${doc.id}','${doc.data().Sunombre}','${doc.data().Suapellido}','${doc.data().Suedad}')">Editar</button></td>
       </tr>
       `
       });
   });
+
+   //borrar documento
+   function eliminar(id){
+    db.collection("miColeccion").doc(id).delete().then(() => {
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+}
+
+
+//Editar Documento
+function editar(id, nombre, apellido, edad){
+    console.log(id);
+    var nombre = document.getElementById('nombre').value = nombre;
+    var apellido = document.getElementById('apellido').value = apellido;
+    var edad = document.getElementById('edad').value = edad;
+    var boton = document.getElementById('boton');
+    boton.innerHTML = 'Editar';
+
+    boton.onclick = function(){
+        var datosaEditar = db.collection("miColeccion").doc(id);
+        var nombre = document.getElementById('nombre').value;
+        var apellido = document.getElementById('apellido').value;
+        var edad = document.getElementById('edad').value;
+
+        // Set the "capital" field of the city 'DC'
+        return datosaEditar.update({
+            Sunombre: nombre,
+            Suapellido: apellido,
+            Suedad: edad
+        })
+        .then(() => {
+            console.log("Document successfully updated!");
+            boton.innerHTML = 'Agregar Dato';
+            window.location.reload();
+        })
+        .catch((error) => {
+            // The document probably doesn't exist.
+            console.error("Error updating document: ", error);
+        });
+    }
+}
